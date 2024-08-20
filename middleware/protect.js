ebaae3729c0e76bcc50680a16ceb9b9440537675
muhzinmuhammed <muhzinsidhiq333@gmail.com>
2024-08-20 18:00:05 +0530

    implemet token middleware

--- middleware/protect.js
+++ middleware/protect.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user/userModel.js";
+import User from "../model/userModel.js";
 
 import * as dotenv from "dotenv";
 
@@ -37,38 +37,6 @@ const protect = async (req, res, next) => {
     res.status(401).json({ error: "No token provided" });
   }
 };
-const protectadmin = async (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Use optional chaining to handle potential undefined headers
 
-  const JWT_SECRET = process.env.JWT_SECRET; // Assuming JWT_SECRET is a string in your .env file
 
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, JWT_SECRET);
-
-      const userId = decoded.user_id;
-
-      const user = await adminpanleUser.findById(userId).select("-password");
-      console.log("dhf",user)
-
-      if (user) {
-        req.user = user;
-        next();
-      } else {
-        res.status(404).json({ error: "User not found" });
-      }
-    } catch (error) {
-      // Check for specific error types like TokenExpiredError, JsonWebTokenError, etc.
-      if (error.name === "TokenExpiredError") {
-        res.status(401).json({ error: "Token expired" });
-      } else {
-        res.status(401).json({ error: "Not authorized, invalid token" });
-      }
-    }
-  } else {
-    // No token provided
-    res.status(401).json({ error: "No token provided" });
-  }
-};
-
-export { protect, protectadmin };
+export { protect };
--- router/postRouter.js
+++ router/postRouter.js
@@ -1,23 +1,23 @@
 import express from 'express'
 import { createPost, getAllPosts, getPostById, editPost, deletePost } from '../controller/postController.js'
-
+import {protect} from '../middleware/protect.js'
 const router = express.Router()
 
 //create post
-router.post('/create_post', createPost)
+router.post('/create_post',protect, createPost)
 
 //get all posts
-router.get('/get_all_posts', getAllPosts)
+router.get('/get_all_posts',protect, getAllPosts)
 
 //get post by id
-router.get('/get_post_by_id/:id', getPostById)
+router.get('/get_post_by_id/:id',protect, getPostById)
 
 
 //edit post
-router.put('/edit_post/:id', editPost)
+router.put('/edit_post/:id',protect, editPost)
 
 //delete post
-router.put('/delete_post/:id', deletePost)
+router.put('/delete_post/:id',protect, deletePost)
 
 export default router
 
